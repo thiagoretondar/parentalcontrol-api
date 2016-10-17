b04d3f6be25f039824a73bdccac408e76d71131b
Thiago Retondar <t.retondar@gmail.com>
2016-10-17 21:31:43 -0200

    Adding new field with user id

--- src/main/java/fei/tcc/dto/AllAppsInfoDto.java
+++ src/main/java/fei/tcc/dto/AllAppsInfoDto.java
@@ -12,10 +12,21 @@ public class AllAppsInfoDto implements Serializable {
     private static final long serialVersionUID = -4294330496906557133L;
 
     @NotNull
+    private Long userId;
+
+    @NotNull
     private List<AppUsageInfoDto> appUsageInfoList;
 
     @NotNull
     private List<LocationInfoDto> locationInfoList;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
     public List<AppUsageInfoDto> getAppUsageInfoList() {
         return appUsageInfoList;
